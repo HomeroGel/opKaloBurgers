ef65a1c1f0e41a355cc7d44b03581cdb355342c3
HomeroGel <homerogelves@gmail.com>
2022-06-04 11:43:37 -0300

    agregue las opiniones de forma dinamica con props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import './styles/App.css';
 function App() {
   return (
     <div className="App">
-      <Testimonio/>
+      <div className='contenedor-principal'>
+        <h1>Esto es lo que opinan nuestros comensales sobre las Kaloburger</h1>
+        <Testimonio
+          nombre='Lucas Rodriguez'
+          imagen='simple'
+          ubicacion='San Andres de Giles'
+          opinion='since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting'/>
+        <Testimonio
+          nombre='Alan Gomez'
+          imagen='dobleBacon'
+          ubicacion='Mercedes'
+          opinion='since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting 1500s, when an unknown printer took a galley of type and scrambled it 1500s, when an unknown printer took a galley of type and scrambled it'/>
+        <Testimonio
+          nombre='Camila Dominic'
+          imagen='dobleCebolla'
+          ubicacion='San Antonio de Areco'
+          opinion='since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting ook a galley of type and scrambled it to make ook a galley of type and scrambled it to make '/>
+      </div>
     </div>
   );
 }
--- src/components/Testimonios.js
+++ src/components/Testimonios.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import '../styles/Testimonios.css'
 
-function Testimonio() {
+function Testimonio(props) {
     return (
         <div className='contenedor-testimonios'>
             <img
                 className='imagen-testimonios'
-                src={require ('../images/hamburguesa-simple.jpeg')}
+                src={require (`../images/hamburguesa-${props.imagen}.jpeg`)}
                 alt='foto-hamburguesa-simple'
             />
             <div className='contenedor-texto-testimonio'>
-                <p className='nombre-testimonio'>Lucas Rodriguez</p>
-                <p className='ubicacion-testimonio'>San Andres de Giles</p>
-                <p className='opinion-testimonio'>Probe las kaloburger y la verdad que quede fascinado, morder y sentir esa explosion de sabores genera una adrenalina en mi cuerpo imposible de explicar, solo los que la prueban saben de lo que hablo. 10 de 10! a galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes</p>
+                <div className='contenedor-nombre-ubicacion'>
+                    <p className='nombre-testimonio'>{props.nombre}</p>
+                    <p className='ubicacion-testimonio'>{props.ubicacion}</p>
+                </div>
+                <p className='opinion-testimonio'>"{props.opinion}"</p>
             </div>
         </div>
     );
